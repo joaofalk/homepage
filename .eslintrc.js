4f93d8925ad3070de76d326c17aa7092f32254a9
Joao Falk <joao.gabriel.falk@gmail.com>
2022-04-24 21:45:23 -0300

    feat: initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  "root": true,
+  "extends": "next",
+  "rules": {
+    "no-unused-vars": [
+      "error",
+      {
+        "argsIgnorePattern": "^_",
+        "varsIgnorePattern": "^_"
+      }
+    ]
+  }
+}
