0dfa1b71912062e23b34677e949dba727c03c535
Joao Falk <joao.gabriel.falk@gmail.com>
2022-04-25 22:18:02 -0300

    feat: added profile image

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,14 @@
-import { Box, Container, Heading } from "@chakra-ui/react"
+import { Box, Container, Heading, Image, useColorModeValue } from "@chakra-ui/react"
 
 const Page = () => {
     return (
         <Container>
-            <Box border="lg" bg="red" p={3} mb={6} align="center"> 
+            <Box 
+            border="lg"
+            bg={useColorModeValue('whiteAlpha.500', 'whiteAlpha.200')}
+            p={3}
+            mb={6}
+            align="center"> 
                 Hello, I'm a full-stack developer based in Brazil
             </Box>
             <Box display={{md:'flex'}}>
@@ -12,6 +17,21 @@ const Page = () => {
                         João Falk
                     </Heading>
                     <p>Full-Stack Developer</p>
+                </Box>
+                <Box 
+                flexShrink={0}
+                mt={{ base: 4, md: 0 }}
+                ml={{ md: 6 }}
+                align= "center">
+                    <Image
+                    borderColor="whiteAlpha.800"
+                    borderWidth={2}
+                    borderStyle="solid"
+                    maxWidth="100px"
+                    display="inline-block"
+                    borderRadius="full"
+                    src="/images/snow.jpg"
+                    alt="Profile Image"/>
                 </Box>
             </Box>
         </Container>
