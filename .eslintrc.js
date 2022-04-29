a33cae1c08216f1031eceab71b265e005df5387f
Joao Falk <joao.gabriel.falk@gmail.com>
2022-04-28 22:59:38 -0300

    fix: fixed prettier formatting

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,12 @@
 module.exports = {
-  "root": true,
-  "extends": "next",
-  "rules": {
-    "no-unused-vars": [
-      "error",
+  root: true,
+  extends: 'next',
+  rules: {
+    'no-unused-vars': [
+      'error',
       {
-        "argsIgnorePattern": "^_",
-        "varsIgnorePattern": "^_"
+        argsIgnorePattern: '^_',
+        varsIgnorePattern: '^_'
       }
     ]
   }
--- pages/index.js
+++ pages/index.js
@@ -1,64 +1,87 @@
-import { ChevronRightIcon } from "@chakra-ui/icons"
-import { Box, Button, Container, Heading, Image, Link, useColorModeValue } from "@chakra-ui/react"
+import { ChevronRightIcon } from '@chakra-ui/icons'
+import {
+  Box,
+  Button,
+  Container,
+  Heading,
+  Image,
+  Link,
+  useColorModeValue
+} from '@chakra-ui/react'
 import NextLink from 'next/link'
-import Paragraph from "../components/paragraph"
-import Section from "../components/section"
+import { BioSection, BioYear } from '../components/bio'
+import Paragraph from '../components/paragraph'
+import Section from '../components/section'
 
 const Page = () => {
-    return (
-        <Container>
-            <Box 
-            border="lg"
-            bg={useColorModeValue('whiteAlpha.500', 'whiteAlpha.200')}
-            p={3}
-            mb={6}
-            align="center"> 
-                Hello, I'm a full-stack developer based in Brazil
-            </Box>
-            <Box display={{md:'flex'}}>
-                <Box flexGrow={1}>
-                    <Heading as="h2" variant="page-title">
-                        João Falk
-                    </Heading>
-                    <p>Full-Stack Developer</p>
-                </Box>
-                <Box 
-                flexShrink={0}
-                mt={{ base: 4, md: 0 }}
-                ml={{ md: 6 }}
-                align= "center">
-                    <Image
-                    borderColor="whiteAlpha.800"
-                    borderWidth={2}
-                    borderStyle="solid"
-                    maxWidth="100px"
-                    display="inline-block"
-                    borderRadius="full"
-                    src="/images/snow.jpg"
-                    alt="Profile Image"/>
-                </Box>
-            </Box>
-        <Section delay={0.1}>
-            <Heading as="h3" variant="section-title" mb={3}>
-                Work
-            </Heading>
-            <Paragraph>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer turpis arcu, congue eu mauris eget, commodo viverra ligula. Donec magna sem, tincidunt eu molestie sed, egestas non nisl. Duis{' '}
-                <NextLink href="/works/inkdrop">
-                    <Link>
-                        quam
-                    </Link>
-                </NextLink>
-                .
-            </Paragraph>
-            <Box align="center" my={4}>
-                <NextLink href="/works">
-                    <Button rightIcon={<ChevronRightIcon/>} colorScheme="teal">My portfolio</Button>
-                </NextLink>
-            </Box>
-        </Section>
-        </Container>
-    )
+  return (
+    <Container>
+      <Box
+        border="lg"
+        bg={useColorModeValue('whiteAlpha.500', 'whiteAlpha.200')}
+        p={3}
+        mb={6}
+        align="center"
+      >
+        Hello, I'm a full-stack developer based in Brazil
+      </Box>
+      <Box display={{ md: 'flex' }}>
+        <Box flexGrow={1}>
+          <Heading as="h2" variant="page-title">
+            João Falk
+          </Heading>
+          <p>Full-Stack Developer</p>
+        </Box>
+        <Box
+          flexShrink={0}
+          mt={{ base: 4, md: 0 }}
+          ml={{ md: 6 }}
+          align="center"
+        >
+          <Image
+            borderColor="whiteAlpha.800"
+            borderWidth={2}
+            borderStyle="solid"
+            maxWidth="100px"
+            display="inline-block"
+            borderRadius="full"
+            src="/images/snow.jpg"
+            alt="Profile Image"
+          />
+        </Box>
+      </Box>
+      <Section delay={0.1}>
+        <Heading as="h3" variant="section-title" mb={3}>
+          Work
+        </Heading>
+        <Paragraph>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer
+          turpis arcu, congue eu mauris eget, commodo viverra ligula. Donec
+          magna sem, tincidunt eu molestie sed, egestas non nisl. Duis{' '}
+          <NextLink href="/works/inkdrop">
+            <Link>quam</Link>
+          </NextLink>
+          .
+        </Paragraph>
+        <Box align="center" my={4}>
+          <NextLink href="/works">
+            <Button rightIcon={<ChevronRightIcon />} colorScheme="teal">
+              My portfolio
+            </Button>
+          </NextLink>
+        </Box>
+      </Section>
+      <Section delay={0.2}>
+        <Heading as="h3" variant="section-title" mb={3}>
+          Bio
+        </Heading>
+        <BioSection>
+          <BioYear>1999</BioYear>
+          Born in Florianópolis, SC - Brazil
+        </BioSection>
+      </Section>
+    </Container>
+  )
 }
 
 export default Page
