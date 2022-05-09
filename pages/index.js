688a53265ff0a862ffc90559325bba99165837d4
Joao Falk <joao.gabriel.falk@gmail.com>
2022-05-09 00:08:01 -0300

    feat: added layout component

--- pages/index.js
+++ pages/index.js
@@ -10,102 +10,105 @@ import {
 } from '@chakra-ui/react'
 import NextLink from 'next/link'
 import { BioSection, BioYear } from '../components/bio'
+import Layout from '../components/layouts/article'
 import Paragraph from '../components/paragraph'
 import Section from '../components/section'
 
 const Page = () => {
   return (
-    <Container>
-      <Box
-        border="lg"
-        bg={useColorModeValue('whiteAlpha.500', 'whiteAlpha.200')}
-        p={3}
-        mb={6}
-        align="center"
-      >
-        Lorem ipsum dolor sit amet, consectetur adipiscing.
-      </Box>
-      <Box display={{ md: 'flex' }}>
-        <Box flexGrow={1}>
-          <Heading as="h2" variant="page-title">
-            Your Name
-          </Heading>
-          <p>Lorem ipsum dolor.</p>
-        </Box>
+    <Layout>
+      <Container>
         <Box
-          flexShrink={0}
-          mt={{ base: 4, md: 0 }}
-          ml={{ md: 6 }}
+          border="lg"
+          bg={useColorModeValue('whiteAlpha.500', 'whiteAlpha.200')}
+          p={3}
+          mb={6}
           align="center"
         >
-          <Image
-            borderColor="whiteAlpha.800"
-            borderWidth={2}
-            borderStyle="solid"
-            maxWidth="100px"
-            display="inline-block"
-            borderRadius="full"
-            src="/images/snow.jpg"
-            alt="Profile Image"
-          />
+          Lorem ipsum dolor sit amet, consectetur adipiscing.
         </Box>
-      </Box>
-      <Section delay={0.1}>
-        <Heading as="h3" variant="section-title" mb={3}>
-          Work
-        </Heading>
-        <Paragraph>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer
-          turpis arcu, congue eu mauris eget, commodo viverra ligula. Donec
-          magna sem, tincidunt eu molestie sed, egestas non nisl. Duis{' '}
-          <NextLink href="/works/inkdrop">
-            <Link>quam</Link>
-          </NextLink>
-          .
-        </Paragraph>
-        <Box align="center" my={4}>
-          <NextLink href="/works">
-            <Button rightIcon={<ChevronRightIcon />} colorScheme="teal">
-              My portfolio
-            </Button>
-          </NextLink>
+        <Box display={{ md: 'flex' }}>
+          <Box flexGrow={1}>
+            <Heading as="h2" variant="page-title">
+              Your Name
+            </Heading>
+            <p>Lorem ipsum dolor.</p>
+          </Box>
+          <Box
+            flexShrink={0}
+            mt={{ base: 4, md: 0 }}
+            ml={{ md: 6 }}
+            align="center"
+          >
+            <Image
+              borderColor="whiteAlpha.800"
+              borderWidth={2}
+              borderStyle="solid"
+              maxWidth="100px"
+              display="inline-block"
+              borderRadius="full"
+              src="/images/snow.jpg"
+              alt="Profile Image"
+            />
+          </Box>
         </Box>
-      </Section>
-      <Section delay={0.2}>
-        <Heading as="h3" variant="section-title" mb={3}>
-          Bio
-        </Heading>
-        <BioSection>
-          <BioYear>1999</BioYear>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis
-          suscipit.
-        </BioSection>
-        <BioSection>
-          <BioYear>2017</BioYear>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus
-          non.
-        </BioSection>
-        <BioSection>
-          <BioYear>2020</BioYear>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam
-          fermentum.
-        </BioSection>
-        <BioSection>
-          <BioYear>2021</BioYear>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras ipsum.
-        </BioSection>
-      </Section>
-      <Section delay={0.2}>
-        <Heading as="h3" variant="section-title" mb={3}>
-          I ♥
-        </Heading>
-        <Paragraph>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer
-          turpis arcu, congue eu mauris eget, commodo viverra ligula. Donec
-          magna sem, tincidunt eu molestie sed, egestas non nisl. Duis quam.
-        </Paragraph>
-      </Section>
-    </Container>
+        <Section delay={0.1}>
+          <Heading as="h3" variant="section-title" mb={3}>
+            Work
+          </Heading>
+          <Paragraph>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer
+            turpis arcu, congue eu mauris eget, commodo viverra ligula. Donec
+            magna sem, tincidunt eu molestie sed, egestas non nisl. Duis{' '}
+            <NextLink href="/works/inkdrop">
+              <Link>quam</Link>
+            </NextLink>
+            .
+          </Paragraph>
+          <Box align="center" my={4}>
+            <NextLink href="/works">
+              <Button rightIcon={<ChevronRightIcon />} colorScheme="teal">
+                My portfolio
+              </Button>
+            </NextLink>
+          </Box>
+        </Section>
+        <Section delay={0.2}>
+          <Heading as="h3" variant="section-title" mb={3}>
+            Bio
+          </Heading>
+          <BioSection>
+            <BioYear>1999</BioYear>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis
+            suscipit.
+          </BioSection>
+          <BioSection>
+            <BioYear>2017</BioYear>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus
+            non.
+          </BioSection>
+          <BioSection>
+            <BioYear>2020</BioYear>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam
+            fermentum.
+          </BioSection>
+          <BioSection>
+            <BioYear>2021</BioYear>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras ipsum.
+          </BioSection>
+        </Section>
+        <Section delay={0.2}>
+          <Heading as="h3" variant="section-title" mb={3}>
+            I ♥
+          </Heading>
+          <Paragraph>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer
+            turpis arcu, congue eu mauris eget, commodo viverra ligula. Donec
+            magna sem, tincidunt eu molestie sed, egestas non nisl. Duis quam.
+          </Paragraph>
+        </Section>
+      </Container>
+    </Layout>
   )
 }
 
--- pages/works.js
+++ pages/works.js
@@ -1,39 +1,42 @@
 import { Container, Divider, Heading, SimpleGrid } from '@chakra-ui/react'
 import { WorkGridItem } from '../components/grid-item'
+import Layout from '../components/layouts/article'
 import Section from '../components/section'
 import thumbInkdrop from '../public/images/atom.jpg'
 
 const Works = () => {
   return (
-    <Container>
-      <Heading as="h3" fontSize={20} mb={4}>
-        Works
-      </Heading>
+    <Layout>
+      <Container>
+        <Heading as="h3" fontSize={20} mb={4}>
+          Works
+        </Heading>
 
-      <SimpleGrid columns={[1, 1, 2]} gap={6}>
-        <Section delay={0.3}>
-          <WorkGridItem id="ipsum" title="Lorem" thumbnail={thumbInkdrop}>
-            Lorem ipsum dolor sis amet.
-          </WorkGridItem>
-        </Section>
-        <Section delay={0.3}>
-          <WorkGridItem id="ipsum" title="Ipsum" thumbnail={thumbInkdrop}>
-            Lorem ipsum dolor sis amet.
-          </WorkGridItem>
-        </Section>
-        <Section delay={0.4}>
-          <Divider my={6} />
-          <Heading as="h3" fontSize={20} mb={4}>
-            Old works
-          </Heading>
-          <Section delay={0.4}>
-            <WorkGridItem id="dolor" title="Dolor" thumbnail={thumbInkdrop}>
+        <SimpleGrid columns={[1, 1, 2]} gap={6}>
+          <Section delay={0.3}>
+            <WorkGridItem id="ipsum" title="Lorem" thumbnail={thumbInkdrop}>
               Lorem ipsum dolor sis amet.
             </WorkGridItem>
           </Section>
-        </Section>
-      </SimpleGrid>
-    </Container>
+          <Section delay={0.3}>
+            <WorkGridItem id="ipsum" title="Ipsum" thumbnail={thumbInkdrop}>
+              Lorem ipsum dolor sis amet.
+            </WorkGridItem>
+          </Section>
+          <Section delay={0.4}>
+            <Divider my={6} />
+            <Heading as="h3" fontSize={20} mb={4}>
+              Old works
+            </Heading>
+            <Section delay={0.4}>
+              <WorkGridItem id="dolor" title="Dolor" thumbnail={thumbInkdrop}>
+                Lorem ipsum dolor sis amet.
+              </WorkGridItem>
+            </Section>
+          </Section>
+        </SimpleGrid>
+      </Container>
+    </Layout>
   )
 }
 
