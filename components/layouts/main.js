b4ad4ad4ca11f290fa91d223cbf5573923663275
Joao Falk <joao.gabriel.falk@gmail.com>
2022-05-11 21:02:50 -0300

    fix: changed name to placeholder text

--- components/layouts/main.js
+++ components/layouts/main.js
@@ -7,7 +7,7 @@ const Main = ({ children, router }) => {
     <Box as="main" pb={8}>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <title>João Falk - Homepage</title>
+        <title>Lorem Ipsum - Homepage</title>
       </Head>
 
       <Navbar path={router.asPath} />
--- components/logo.js
+++ components/logo.js
@@ -31,7 +31,7 @@ const Logo = () => {
             fontWeight="bold"
             ml={3}
           >
-            João Falk
+            Lorem Ipsum
           </Text>
         </LogoBox>
       </a>
